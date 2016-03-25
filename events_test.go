ed83d8deb5df74400bc866f8ed7b9f9bdf8ea166
Mike Johnston <mike.qbox.io>
2016-03-25 15:46:39 -0500

    Added GoConvey test for event .Query()

--- events_test.go
+++ events_test.go
@@ -71,11 +71,13 @@ func TestEventsKind(t *testing.T) {
 func TestEventsCreate(t *testing.T) {
 	Convey("We start a mock api server.", t, func() {
 		ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// TODO: Should we mock a full response here, and should a kubernetes lib be aware of a malformed response?
+			// Right now we seem to accept any successful status code.
 			io.WriteString(w, "Much Success")
 		}))
 		defer ts.Close()
 		So(ts, ShouldNotBeNil)
-		Convey("When calling the .Create(&Event) method on a Client object.", func() {
+		Convey("When calling the .Create(&Event) method on an Events object.", func() {
 			tClient.Host = strings.Replace(ts.URL, "https://", "", -1)
 			resp, _ := tevents.Create(&Event{
 				Message: "test",
@@ -90,73 +92,53 @@ func TestEventsCreate(t *testing.T) {
 			})
 		})
 	})
-}
-
-func TestCreateError(t *testing.T) {
-	_, err := tevents.Create(&Event{
-		Message: "test",
-		Count:   1,
+	Convey("When calling the .Create(&Event) method on a Client object. And there is a problem. We should get an error.", t, func() {
+		_, err := tevents.Create(&Event{
+			Message: "test",
+			Count:   1,
+		})
+		So(err, ShouldNotBeNil)
 	})
-
-	if err == nil {
-		t.Error("ERROR .Create(): event create to fail.. but it did not. ")
-	}
+	tClient.Host = "test"
 }
 
-func TestQuery(t *testing.T) {
-	// Setup our test server
-	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, `{
-  "kind": "EventList",
-  "apiVersion": "v1",
-  "metadata": {
-    "selfLink": "/api/v1/events",
-    "resourceVersion": "test"
-  },
-  "items": [
-	{
-            "message": "test"
-        }
-	]
+func TestEventQuery(t *testing.T) {
+	Convey("We start a mock api server.", t, func() {
+		ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			io.WriteString(w, `{
+   "kind":"EventList",
+   "apiVersion":"v1",
+   "metadata":{
+      "selfLink":"/api/v1/events",
+      "resourceVersion":"test"
+   },
+   "items":[
+      {
+         "message":"test"
+      }
+   ]
 }`)
-	}))
-	defer ts.Close()
-
-	// test client
-	url := strings.Replace(ts.URL, "https://", "", -1)
-	tsClient := &Client{
-		Host:     url,
-		Username: "test",
-		Password: "test",
-		http: &http.Client{
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true,
-				},
-			},
-		},
-	}
-
-	tsEvents := &Events{
-		client:    tsClient,
-		Namespace: "test",
-	}
-	resp, _ := tsEvents.Query(&QueryParams{
-		LabelSelector: "test",
+		}))
+		defer ts.Close()
+		So(ts, ShouldNotBeNil)
+		Convey("When calling the .Query(&QueryParams) method on an Events object that exists.", func() {
+			tClient.Host = strings.Replace(ts.URL, "https://", "", -1)
+			resp, _ := tevents.Query(&QueryParams{
+				LabelSelector: "test",
+			})
+			Convey("The response object should be a EventsList containing the messge value \"test\" from our server response.", func() {
+				So(resp.Items[0].Message, ShouldEqual, "test")
+			})
+		})
 	})
-
-	if resp.Items[0].Message != "test" {
-		t.Error("ERROR .Query(): expected, \"test\"  -- But got,", resp.Items[0].Message)
-	}
-}
-
-func TestQueryError(t *testing.T) {
-	_, err := tevents.Query(&QueryParams{
-		LabelSelector: "test",
+	Convey("When calling the .Query(&QueryParams) method on an Events object. And there is an error", t, func() {
+		_, err := tevents.Query(&QueryParams{
+			LabelSelector: "Cheese",
+		})
+		Convey("The response should be nil, and there should be an error.", func() {
+			So(err, ShouldNotBeNil)
+		})
 	})
-	if err == nil {
-		t.Error("ERROR .Create(): event create to fail.. but it did not. ")
-	}
 }
 
 func TestList(t *testing.T) {
