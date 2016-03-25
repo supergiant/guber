17491051904edcfba0d0c1a0be5e5895d193f753
spankenstein <spankenstien@gmail.com>
2016-03-24 21:48:54 -0500

    More testing with events.go
    Added tests for Get, List, Query.

--- events_test.go
+++ events_test.go
@@ -113,3 +113,191 @@ func TestCreateError(t *testing.T) {
 		t.Error("ERROR .Create(): event create to fail.. but it did not. ")
 	}
 }
+
+func TestQuery(t *testing.T) {
+	// Setup our test server
+	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, `{
+  "kind": "EventList",
+  "apiVersion": "v1",
+  "metadata": {
+    "selfLink": "/api/v1/events",
+    "resourceVersion": "test"
+  },
+  "items": [
+	{
+            "message": "test"
+        }
+	]
+}`)
+	}))
+	defer ts.Close()
+
+	// test client
+	url := strings.Replace(ts.URL, "https://", "", -1)
+	tsClient := &Client{
+		Host:     url,
+		Username: "test",
+		Password: "test",
+		http: &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: true,
+				},
+			},
+		},
+	}
+
+	tsEvents := &Events{
+		client:    tsClient,
+		Namespace: "test",
+	}
+	resp, _ := tsEvents.Query(&QueryParams{
+		LabelSelector: "test",
+	})
+
+	if resp.Items[0].Message != "test" {
+		t.Error("ERROR .Query(): expected, \"test\"  -- But got,", resp.Items[0].Message)
+	}
+}
+
+func TestQueryError(t *testing.T) {
+	_, err := tevents.Query(&QueryParams{
+		LabelSelector: "test",
+	})
+	if err == nil {
+		t.Error("ERROR .Create(): event create to fail.. but it did not. ")
+	}
+}
+
+func TestList(t *testing.T) {
+	// Setup our test server
+	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, `{
+  "kind": "EventList",
+  "apiVersion": "v1",
+  "metadata": {
+    "selfLink": "/api/v1/events",
+    "resourceVersion": "test"
+  },
+  "items": [
+	{
+            "message": "test"
+        }
+	]
+}`)
+	}))
+	defer ts.Close()
+
+	// test client
+	url := strings.Replace(ts.URL, "https://", "", -1)
+	tsClient := &Client{
+		Host:     url,
+		Username: "test",
+		Password: "test",
+		http: &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: true,
+				},
+			},
+		},
+	}
+
+	tsEvents := &Events{
+		client:    tsClient,
+		Namespace: "test",
+	}
+	resp, _ := tsEvents.List()
+
+	if resp.Items[0].Message != "test" {
+		t.Error("ERROR .List(): expected, \"test\"  -- But got,", resp.Items[0].Message)
+	}
+}
+
+func TestListError(t *testing.T) {
+	_, err := tevents.List()
+	if err == nil {
+		t.Error("ERROR .List(): event create to fail.. but it did not. ")
+	}
+}
+
+func TestEventGet(t *testing.T) {
+	// Setup our test server
+	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, `{
+            "kind": "Event",
+            "apiVersion": "v1",
+            "metadata": {
+                "name": "test"
+            },
+            "message": "test"
+        }`)
+	}))
+	defer ts.Close()
+
+	// test client
+	url := strings.Replace(ts.URL, "https://", "", -1)
+	tsClient := &Client{
+		Host:     url,
+		Username: "test",
+		Password: "test",
+		http: &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: true,
+				},
+			},
+		},
+	}
+
+	tsEvents := &Events{
+		client:    tsClient,
+		Namespace: "test",
+	}
+	resp, _ := tsEvents.Get("test")
+
+	if resp.Message != "test" {
+		t.Error("ERROR .Get(): expected, \"test\"  -- But got,", resp.Message)
+	}
+}
+
+func TestEventGetNilResult(t *testing.T) {
+	// Setup our test server
+	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(666)
+		io.WriteString(w, "")
+	}))
+	defer ts.Close()
+
+	// test client
+	url := strings.Replace(ts.URL, "https://", "", -1)
+	tsClient := &Client{
+		Host:     url,
+		Username: "test",
+		Password: "test",
+		http: &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: true,
+				},
+			},
+		},
+	}
+
+	tsEvents := &Events{
+		client:    tsClient,
+		Namespace: "test",
+	}
+	resp, _ := tsEvents.Get("test")
+
+	if resp != nil {
+		t.Error("ERROR .Get(): expected, Nil  -- But... I have no idea... ")
+	}
+}
+func TestGetError(t *testing.T) {
+	_, err := tevents.Get("test")
+	if err == nil {
+		t.Error("ERROR .Get(): event create to fail.. but it did not. ")
+	}
+}
